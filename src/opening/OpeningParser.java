836afd0deea3c7312df10bbe7e1f5e06a07f6486
Stefania-Elena GHERASIE <stefania.gherasie@stud.acs.upb.ro>
2020-04-16 22:36:35 +0300

    opening 2

--- src/opening/OpeningParser.java
+++ src/opening/OpeningParser.java
@@ -1,5 +1,5 @@
 package opening;
 
 public class OpeningParser {
-
+	
 }
---
+++ src/opening/OpeningStrategy.java
@@ -0,0 +1,10 @@
+package opening;
+import auxiliary.*;
+import main.*;
+import commands.*;
+import pieces.*;
+import java.util.*;
+
+public class OpeningStrategy {
+	private Map<ChessBoard, List<OpeningMove>> positions;
+}
